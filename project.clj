80fdba21e82b16057a35641feb7ad60d8b4d5e5e
Chris Dean <ctdean@sokitomi.com>
2015-07-07 16:43:31 -0700

    Add migrations to clams.
    - This is a lot of functionality, so bumped the minor number
    - Supports SQL and Mongo, so added those jars to the deps
    - Docs in https://github.com/standardtreasury/clams/wiki/Migrations
    - Yes, this works in an uber jar

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clams "0.1.2"
+(defproject clams "0.2.0"
   :description "Clojure with Clams. A framework for web apps."
   :url "https://github.com/standardtreasury/clams"
   :license {:name "The MIT License"
@@ -6,13 +6,17 @@
   :dependencies
     [
      [clout "2.0.0"]
+     [com.novemberain/monger "3.0.0-rc2"]
      [compojure "1.2.0"]
      [http-kit "2.1.19"]
      [metosin/ring-http-response "0.5.2"]
      [org.clojure/clojure "1.6.0"]
      [org.clojure/tools.macro "0.1.5"]
+     [org.postgresql/postgresql "9.3-1102-jdbc41"]
      [prismatic/schema "0.3.1"]
+     [ragtime "0.4.1"]
      [ring "1.3.1"]
      [ring-mock "0.1.5"]
      [ring/ring-json "0.3.1"]
-    ])
+     ]
+    :aot [clams.migrate])
---
+++ test/clams/test/migrate_test.clj
@@ -0,0 +1,50 @@
+(ns clams.test.migrate-test
+  (:require
+   [clojure.test :refer :all]
+   [clams.migrate :as migrate]
+   [ragtime.jdbc :as jdbc]
+   ))
+
+(deftest read-empty-configs-test
+  (is (nil? (#'migrate/sql-config)))
+  (is (nil? (#'migrate/mongo-config)))
+  )
+
+(def fake-migrations
+  [{:scheme :mongo
+    :id "001-foo"
+    :up (constantly "foo")
+    :down (constantly "foo")}
+   {:scheme :mongo
+    :id "002-bar"
+    :up (constantly "bar")
+    :down (constantly "bar")}
+   {:scheme :jdbc
+    :id "003-baz"
+    :up (constantly "baz")
+    :down (constantly "baz")}])
+
+(def fake-conf {:mongo-url "mongo url"})
+
+(deftest mongo-config-test
+  (with-redefs [ragtime.jdbc/load-resources (constantly fake-migrations)
+                monger.core/connect-via-uri (constantly {:db "mongo handle"})
+                clams.conf/get (fn [k] (get fake-conf k))]
+    (is (= {:database "mongo handle" :migrations (take 2 fake-migrations)}
+           (#'migrate/mongo-config)))
+
+    (let [state (atom nil)]
+      (is (= nil
+             (#'migrate/run-migrations (fn [c] (swap! state conj c)))))
+      (is (= [{:database "mongo handle" :migrations (take 2 fake-migrations)}]
+             @state)))
+    ))
+
+(deftest load-files-test
+  (let [migrations (jdbc/load-files ["/abc/migrate/001-foo.up.mongoclj"
+                                     "/abc/migrate/001-foo.down.mongoclj"
+                                     "/abc/migrate/002-bar.up.mongoclj"])]
+    (is (= ["001-foo" "002-bar"] (map :id migrations)))
+    (is (= [:mongo] (distinct (map :scheme migrations))))
+    (is (= 2 (count (filter identity (map :up migrations)))))
+    (is (= 2 (count (filter identity (map :down migrations)))))))

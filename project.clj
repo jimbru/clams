7edd09efe9ce7541a8978ae870e8fbff25d36032
Jim Brusstar <jim.brusstar@gmail.com>
2015-09-29 09:56:51 -0700

    Version to 0.2.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clams "0.2.4-SNAPSHOT"
+(defproject clams "0.2.4"
   :description "Clojure with Clams. A framework for web apps."
   :url "https://github.com/standardtreasury/clams"
   :license {:name "The MIT License"

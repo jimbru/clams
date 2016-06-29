55729d220e6a41c31502418e63c2ff49eedf9473
Jim Brusstar <jim.brusstar@gmail.com>
2016-06-28 21:13:09 -0700

    Use external conf library (#24)
    To facilitate easier use in other projects, I broke out clams.conf into a
    standalone library (https://github.com/jimbru/conf). This updates Clams to
    depend on the library version of conf.

--- project.clj
+++ project.clj
@@ -1,23 +1,21 @@
-(defproject clams "0.3.2"
+(defproject clams "0.3.3"
   :description "Clojure with Clams. A framework for web apps."
   :url "https://github.com/standardtreasury/clams"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies
-  [
-     [clout "2.1.2"]
-     [com.novemberain/monger "3.0.0-rc2"]
-     [compojure "1.4.0"]
-     [http-kit "2.1.19"]
-     [metosin/ring-http-response "0.6.5"]
-     [org.clojure/clojure "1.6.0"]
-     [org.clojure/tools.logging "0.3.1"]
-     [org.clojure/tools.macro "0.1.5"]
-     [org.postgresql/postgresql "9.3-1102-jdbc41"]
-     [prismatic/schema "0.3.1"]
-     [ragtime "0.4.1"]
-     [ring "1.4.0"]
-     [ring-mock "0.1.5"]
-     [ring/ring-json "0.4.0"]
-     ]
-    :aot [clams.migrate])
+  :dependencies [[clout "2.1.2"]
+                 [com.novemberain/monger "3.0.0-rc2"]
+                 [compojure "1.4.0"]
+                 [conf "0.9.1" :exclusions [org.clojure/clojure]]
+                 [http-kit "2.1.19"]
+                 [metosin/ring-http-response "0.6.5"]
+                 [org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [org.clojure/tools.macro "0.1.5"]
+                 [org.postgresql/postgresql "9.3-1102-jdbc41"]
+                 [prismatic/schema "0.3.1"]
+                 [ragtime "0.4.1"]
+                 [ring "1.4.0"]
+                 [ring-mock "0.1.5"]
+                 [ring/ring-json "0.4.0"]]
+  :aot [clams.migrate])
